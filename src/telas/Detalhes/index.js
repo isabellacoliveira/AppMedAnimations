efadfd3766f41926e39f28f0dab3fe8eaebecd03
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-06-26 12:01:45 -0300

    fazendo a animação do sininho

--- src/telas/Detalhes/index.js
+++ src/telas/Detalhes/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { TelaDeFundo } from '../../componentes/TelaDeFundo';
 import { InformacoesUsuario } from '../../componentes/InformacoesUsuario';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import mapa from '../../assets/mapa.png';
 import styles from './styles';
-import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, withTiming, withRepeat } from 'react-native-reanimated';
 import { Button } from 'react-native-web';
 
 export default function Detalhes(props) {
   const dados = props.route.params;
 
   // variavel que ao alterar a posição, ela mexerá com o estilo de algum elemento da tela 
-  const posicao = useSharedValue(0);
+  // animação do sininho
+  const rotacao = useSharedValue(0);
+  // o quanto vamos movimentar o sininho 
+  const angulo = -30;
+
+  const [jaAnimou, setJaAnimou] = useState(false);
+
 
   const estiloAnimado = useAnimatedStyle(() => {
     return {
@@ -20,22 +26,29 @@ export default function Detalhes(props) {
         {
           // isso vai fazer o carregamento suave na tela 
           // ao alterar o valor da posicao, alteramos o estilo
-          translateX: withSpring(posicao.value)
+          // precisamos colocar em graus 
+          rotate: `${rotacao.value}deg`
         }
       ]
     }
   })
 
-  function alterarPosicaoBloco(){
-    posicao.value = Math.random() * 255;
+  function fazerRotacao(){
+    // movimentar o sininho
+    // tempo da animação
+    // withRepeat para fazer a repetição
+    // true pois ele vai fazer uma vez e voltar, 6 pois fará 6 vezes  
+    rotacao.value = withRepeat(withTiming(angulo, { duration: 120 }), 6, true)
+
+    // depois de executar determinado tempo 
+    setTimeout(() => {
+      setJaAnimou(true);
+    }, 1000)
   }
 
   return (
     <TelaDeFundo>
       <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-        {/* view com animação embutida nela */}
-        <Animated.View style={[{ backgroundColor: 'green', width: 50, height: 50 }, estiloAnimado]} />
-        <Button title='Mova' onPress={alterarPosicaoBloco} />
         <InformacoesUsuario
           nome={dados.nome}
           detalhes="Cliente desde 2018"
@@ -65,13 +78,20 @@ export default function Detalhes(props) {
           <Text>{dados.endereco}</Text>
           <TouchableOpacity 
             style={styles.botao} 
+            onPress={fazerRotacao}
           >
             <Text style={styles.botaoTexto}>Notificar consulta</Text>
+            {/* a view vai alterar a rotação do icone */}
+            <Animated.View 
+              style={[styles.icone, estiloAnimado]}
+            >
               <Icon 
-                name={'notifications-none'} 
+              // faremos com que ele pinte o icone alternando entre as classes 'notifications' e 'notifications-none'
+                name={jaAnimou ? 'notifications' : 'notifications-none'} 
                 size={20} 
                 color="#FFF"
               />
+            </Animated.View>
           </TouchableOpacity>
       </ScrollView>
     </TelaDeFundo>
